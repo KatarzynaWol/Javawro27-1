01eae9128dc4403926fc74df5849f4ded3b4bcb3
KatarzynaWolska <wolska.katarzyna53@gmail.com>
2020-02-23 13:41:04 +0100

    cwiczenia dat

--- src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/DatesMain.java
+++ src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/DatesMain.java
@@ -2,12 +2,21 @@ package pl.sda.rafal.zientara.programowanie2.lesson4;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 public class DatesMain {
     public static void main(String[] args) {
-        LocalDate.now();
-        ZonedDateTime.now();
-        LocalDateTime.now();
+        LocalDate now = LocalDate.now();
+        final ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        final LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(now);
+        System.out.println(zonedDateTime);
+        System.out.println(localDateTime);
+        Set<String> allZones= ZoneId.getAvailableZoneIds();
+        for(String zone:allZones){
+            System.out.println(zone);
+        }
     }
 }
