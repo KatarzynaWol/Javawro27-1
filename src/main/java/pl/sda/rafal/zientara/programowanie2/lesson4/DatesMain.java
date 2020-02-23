521033621f2b09dff29fec5275929851d6bdbf4a
KatarzynaWolska <wolska.katarzyna53@gmail.com>
2020-02-23 13:36:56 +0100

    cwiczenia dat

---
+++ src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/DatesMain.java
@@ -0,0 +1,13 @@
+package pl.sda.rafal.zientara.programowanie2.lesson4;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public class DatesMain {
+    public static void main(String[] args) {
+        LocalDate.now();
+        ZonedDateTime.now();
+        LocalDateTime.now();
+    }
+}
